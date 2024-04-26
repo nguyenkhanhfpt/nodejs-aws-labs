564c57eaf35e6c7917ddc483c5ef050defa22570
nguyenkhanhfpt <khanh26122000@gmail.com>
2024-04-26 14:00:53 +0700

    feat: multipart upload object - refactor

--- s3/multipart_upload.js
+++ s3/multipart_upload.js
@@ -15,69 +15,86 @@ const client = new S3Client({
     secretAccessKey: process.env.SECRET_ACCESS_KEY,
   },
 });
-
 const objectKey = "largeobject.jpg";
-
-let inputMultipartUpload = {
-  Bucket: process.env.BUCKET_NAME,
-  Key: objectKey,
-};
 
 async function main() {
   try {
-    const command = new CreateMultipartUploadCommand(inputMultipartUpload);
-    const response = await client.send(command);
+    console.log("Start send...");
+    const createResponse = await createMultipartUpload();
+    const { UploadId } = createResponse;
 
-    let { UploadId } = response;
-    let objFile = await fileToBuffer("./png-5mb-1.png");
+    let uploadPartResults = await getUploadPartResults(UploadId);
+    console.log("------------------");
+    console.log(uploadPartResults);
 
+    let completeUploadRes = await completeMultipartUpload(UploadId, uploadPartResults);
+    console.log("------------------");
+    console.log(completeUploadRes);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function createMultipartUpload() {
+  const command = new CreateMultipartUploadCommand({
+    Bucket: process.env.BUCKET_NAME,
+    Key: objectKey,
+  });
+
+  return await client.send(command);
+}
+
+const getUploadPartResults = async (UploadId) => {
+  try {
+    const objFile = await fileToBuffer("./png-5mb-1.png");
     const partSize = 1024 * 1024 * 5;
     const parts = Math.ceil(objFile.length / partSize);
-    let uploadParts = [];
+    let uploadPartsPromises = [];
 
     for (let i = 0; i < parts; i++) {
       let start = partSize * i;
       let end = Math.min(start + partSize, objFile.length);
+      let partNumber = i + 1;
 
-      uploadParts.push(
-        client
-          .send(
-            new UploadPartCommand({
-              UploadId,
-              Body: objFile.subarray(start, end),
-              Bucket: process.env.BUCKET_NAME,
-              Key: objectKey,
-              PartNumber: i + 1,
-            })
-          )
-          .then((d) => {
-            console.log("Part", i + 1, "uploaded : ", d);
-
-            return d;
+      let uploadPartsPromise = client
+        .send(
+          new UploadPartCommand({
+            UploadId,
+            Body: objFile.subarray(start, end),
+            Bucket: process.env.BUCKET_NAME,
+            Key: objectKey,
+            PartNumber: partNumber,
           })
-      );
+        )
+        .then((d) => {
+          console.log("Part", partNumber, "uploaded : ", d);
+
+          return d;
+        });
+
+      uploadPartsPromises.push(uploadPartsPromise);
     }
 
-    const uploadPartResults = await Promise.all(uploadParts);
-
-    let uploadResponse = await client.send(
-      new CompleteMultipartUploadCommand({
-        UploadId,
-        Bucket: process.env.BUCKET_NAME,
-        MultipartUpload: {
-          Parts: uploadPartResults.map(({ ETag }, i) => ({
-            ETag,
-            PartNumber: ++i,
-          })),
-        },
-        Key: objectKey,
-      })
-    );
-
-    console.log(uploadResponse);
-  } catch (e) {
-    console.log(e);
+    return await Promise.all(uploadPartsPromises);
+  } catch (error) {
+    throw error;
   }
+};
+
+async function completeMultipartUpload(UploadId, uploadPartResults) {
+  return await client.send(
+    new CompleteMultipartUploadCommand({
+      UploadId,
+      Bucket: process.env.BUCKET_NAME,
+      MultipartUpload: {
+        Parts: uploadPartResults.map(({ ETag }, i) => ({
+          ETag,
+          PartNumber: ++i,
+        })),
+      },
+      Key: objectKey,
+    })
+  );
 }
 
 main();
